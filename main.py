cf90dace9eab38e2ef47be61d2920afb2f90d7e2
ilmossp <ilyamodeni@gmail.com>
2023-10-17 15:30:38 +0100

    fix: Types for flows are now correct

--- main.py
+++ main.py
@@ -1,42 +1,51 @@
 from joblib import load
-from fastapi import FastAPI,Response,status
+from fastapi import FastAPI, Response, status
+from fastapi.middleware.cors import CORSMiddleware
 from pcap import LiveCapture
 import threading
 
 liveCapture = LiveCapture()
 
-#Load the model
-model = load('models/random_forest_model_dropped.joblib') 
+# Load the model
+model = load("models/random_forest_model_dropped.joblib")
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods="*",
+)
+
 
 @app.on_event("startup")
 async def startup_event():
     global thread
-    liveCapture.start_Capture() 
+    liveCapture.start_Capture()
     thread = threading.Thread(target=liveCapture.fill_queue)
     thread.start()
-@app.get('/')
+
+
+@app.get("/")
 def index():
-    return {'message': 'This is the homepage of the API'}
+    return {"message": "This is the homepage of the API"}
 
 
-@app.get('/queue')
-def get_queue(response:Response):
-    if len(liveCapture.queue)<100:
+@app.get("/queue")
+def get_queue(response: Response):
+    if len(liveCapture.queue) < 100:
         response.status_code = status.HTTP_202_ACCEPTED
-        return {"message":"Queue still loading"}
-    return {"size": len(liveCapture.queue),"queue":liveCapture.queue}
+        return {"message": "Queue still loading"}
+    return {"queue": liveCapture.queue}
 
-@app.get('hello')
-def hello():
-    return {"msg": "hello"}
 
-@app.get('/stop')
+@app.get("/stop")
 def stop_capture():
     return liveCapture.stop_Capture()
 
-@app.get('/predict')
+
+@app.get("/predict")
 def predict(body):
-    return {"prediction":model.predict(body)}
-
-
+    return {"prediction": model.predict(body)}
--- pcap.py
+++ pcap.py
@@ -3,13 +3,13 @@ import xml.etree.ElementTree as ET
 from socket import getservbyport
 import datetime
 
+
 # Command 1: argus command that writes data to stdout
-cmd1 = ["sudo","argus", "-i", "wlo1", "-w", "-","|"]
+cmd1 = ["sudo", "argus", "-i", "wlo1", "-w", "-", "|"]
 # Command 2: ra command that analyzes data and writes as XML to stdout
 cmd_string = "ra -s srcid,saddr,sport,daddr,dport,proto,state,dur,sbytes,dbytes,sttl,dttl,service,sload,dload,spkts,dpkts,stcpb,dtcpb,smeansz,dmeansz,trans,sjit,djit,stime,ltime,sintpkt,dintpkt,tcprtt,synack,ackdat -M xml -n"
 cmd2 = cmd_string.split()
 # Start the first command
-
 
 
 # flow_attributes = ['SrcId', 'SrcAddr', 'Sport', 'DstAddr', 'Dport', 'Proto', 'State', 'Dur', 'SrcBytes', 'DstBytes', 'sTtl', 'dTtl', 'SrcLoad', 'DstLoad', 'SrcPkts', 'DstPkts', 'SrcTCPBase', 'DstTCPBase', 'sMeanPktSz', 'dMeanPktSz', 'Trans', 'SrcJitter', 'DstJitter', 'StartTime', 'LastTime', 'SIntPkt', 'DIntPkt', 'TcpRtt', 'SynAck', 'AckDat']
@@ -24,21 +24,24 @@ cmd2 = cmd_string.split()
 
 class LiveCapture:
     queue: list = []
-    process: subprocess.Popen = None # type: ignore
+    process: subprocess.Popen = None  # type: ignore
+
     def start_Capture(self):
         # Start the first command
         argus_capture = subprocess.Popen(cmd1, stdout=subprocess.PIPE, text=True)
-        ra_analysis = subprocess.Popen(cmd2,stdin=argus_capture.stdout,text=True,stdout=subprocess.PIPE)
+        ra_analysis = subprocess.Popen(
+            cmd2, stdin=argus_capture.stdout, text=True, stdout=subprocess.PIPE
+        )
         print("Started Capture")
         self.process = ra_analysis
-    
+
     def stop_Capture(self):
         self.process.terminate()
         return True
-     
+
     def fill_queue(self):
         while True:
-            line = self.process.stdout.readline() # type: ignore
+            line = self.process.stdout.readline()  # type: ignore
             if not line:
                 break
             flow = self.process_output(line)
@@ -46,87 +49,95 @@ class LiveCapture:
                 if len(self.queue) >= 100:
                     self.queue.pop(0)
 
-                #print(flow)
+                # print(flow)
                 self.queue.append(flow)
-        
+
     def process_flow(self, flow):
         ct_state_ttl = 0
-        ct_flw_http_mthd=0
-        ct_srv_src =0
-        ct_srv_dst=0
-        ct_dst_ltm=0
-        ct_src_ltm=0
+        ct_flw_http_mthd = 0
+        ct_srv_src = 0
+        ct_srv_dst = 0
+        ct_dst_ltm = 0
+        ct_src_ltm = 0
 
         service = 0
         try:
-            if 'DstPort' in flow.attrib :
-                if flow.attrib['Proto'].lower() == 'tcp' or flow.attrib['Proto'].lower() == 'udp':
-                    service = getservbyport(int(flow.attrib['DstPort']), flow.attrib['Proto'].lower())
+            if "DstPort" in flow.attrib:
+                if (
+                    flow.attrib["Proto"].lower() == "tcp"
+                    or flow.attrib["Proto"].lower() == "udp"
+                ):
+                    service = getservbyport(
+                        int(flow.attrib["DstPort"]), flow.attrib["Proto"].lower()
+                    )
         except:
             service = 0
-        
-        stime = datetime.datetime.fromisoformat(flow.attrib['StartTime']).timestamp()
-        ltime = datetime.datetime.fromisoformat(flow.attrib['LastTime']).timestamp()
-        
+
+        stime = datetime.datetime.fromisoformat(flow.attrib["StartTime"]).timestamp()
+        ltime = datetime.datetime.fromisoformat(flow.attrib["LastTime"]).timestamp()
+
         for item in self.queue:
             if item["service"] == service:
                 if item["srcip"] == flow.attrib["SrcAddr"]:
-                    ct_srv_src+=1
-                    ct_src_ltm+=1
+                    ct_srv_src += 1
+                    ct_src_ltm += 1
                 if item["dstip"] == flow.attrib["DstAddr"]:
-                    ct_srv_dst+=1
-                    ct_dst_ltm+=1
+                    ct_srv_dst += 1
+                    ct_dst_ltm += 1
             if item["service"] == "http" or item["service"] == "https":
-                ct_flw_http_mthd+=1
-             
+                ct_flw_http_mthd += 1
 
         flow_dict = {
-            'id': flow.attrib['SrcId'] if 'SrcId' in flow.attrib else 0,
-            'srcip': flow.attrib['SrcAddr'] if 'SrcAddr' in flow.attrib else 0,
-            'sport': flow.attrib['SrcPort'] if 'SrcPort' in flow.attrib else 0,
-            'dstip': flow.attrib['DstAddr'] if 'DstAddr' in flow.attrib else 0,
-            'dsport': flow.attrib['DstPort'] if 'DstPort' in flow.attrib else 0,
-            'proto': flow.attrib['Proto'] if 'Proto' in flow.attrib else 0,
-            'state': flow.attrib['State'] if 'State' in flow.attrib else 0,
-            'dur': flow.attrib['Dur'] if 'Dur' in flow.attrib else 0,
-            'sbytes': flow.attrib['SrcBytes'] if 'SrcBytes' in flow.attrib else 0,
-            'dbytes': flow.attrib['DstBytes'] if 'DstBytes' in flow.attrib else 0,
-            'sttl': flow.attrib['sTtl'] if 'sTtl' in flow.attrib else 0,
-            'dttl': flow.attrib['dTtl'] if 'dTtl' in flow.attrib else 0,
-            'service': service,
-            'sload': flow.attrib['SrcLoad'] if 'SrcLoad' in flow.attrib else 0,
-            'dload': flow.attrib['DstLoad'] if 'DstLoad' in flow.attrib else 0,
-            'Spkts': flow.attrib['SrcPkts'] if 'SrcPkts' in flow.attrib else 0,
-            'Dpkts': flow.attrib['DstPkts'] if 'DstPkts' in flow.attrib else 0,
-            'stcpb': flow.attrib['SrcTCPBase'] if 'SrcTCPBase' in flow.attrib else 0,
-            'dtcpb': flow.attrib['DstTCPBase'] if 'DstTCPBase' in flow.attrib else 0,
-            'smean': flow.attrib['sMeanPktSz'] if 'sMeanPktSz' in flow.attrib else 0,
-            'dmean': flow.attrib['dMeanPktSz'] if 'dMeanPktSz' in flow.attrib else 0,
-            'trans_depth': flow.attrib['Trans'] if 'Trans' in flow.attrib else 0,
-            'sjit': flow.attrib['SrcJitter'] if 'SrcJitter' in flow.attrib else 0,
-            'djit': flow.attrib['DstJitter'] if 'DstJitter' in flow.attrib else 0,
-            'Stime': stime,
-            'Ltime': ltime,
-            'Sintpkt': flow.attrib['SIntPkt'] if 'SIntPkt' in flow.attrib else 0,
-            'Dintpkt': flow.attrib['DIntPkt'] if 'DIntPkt' in flow.attrib else 0,
-            'tcprtt': flow.attrib['TcpRtt'] if 'TcpRtt' in flow.attrib else 0,
-            'synack': flow.attrib['SynAck'] if 'SynAck' in flow.attrib else 0,
-            'ackdat': flow.attrib['AckDat'] if 'AckDat' in flow.attrib else 0,
-            'ct_state_ttl': ct_state_ttl,
-            'ct_flw_http_mthd':0,
-            'ct_srv_src': 0,
-            'ct_srv_dst': 0,
-            'ct_dst_ltm': 0,
-            'ct_src_ltm': 0
+            "id": flow.attrib["SrcId"] if "SrcId" in flow.attrib else 0,
+            "srcip": flow.attrib["SrcAddr"] if "SrcAddr" in flow.attrib else 0,
+            "sport": int(flow.attrib["SrcPort"]) if "SrcPort" in flow.attrib else 0,
+            "dstip": flow.attrib["DstAddr"] if "DstAddr" in flow.attrib else 0,
+            "dsport": int(flow.attrib["DstPort"]) if "DstPort" in flow.attrib else 0,
+            "proto": flow.attrib["Proto"] if "Proto" in flow.attrib else 0,
+            "state": flow.attrib["State"] if "State" in flow.attrib else 0,
+            "dur": flow.attrib["Dur"] if "Dur" in flow.attrib else 0,
+            "sbytes": int(flow.attrib["SrcBytes"]) if "SrcBytes" in flow.attrib else 0,
+            "dbytes": int(flow.attrib["DstBytes"]) if "DstBytes" in flow.attrib else 0,
+            "sttl": flow.attrib["sTtl"] if "sTtl" in flow.attrib else 0,
+            "dttl": flow.attrib["dTtl"] if "dTtl" in flow.attrib else 0,
+            "service": service,
+            "sload": float(flow.attrib["SrcLoad"]) if "SrcLoad" in flow.attrib else 0,
+            "dload": float(flow.attrib["DstLoad"]) if "DstLoad" in flow.attrib else 0,
+            "Spkts": int(flow.attrib["SrcPkts"]) if "SrcPkts" in flow.attrib else 0,
+            "Dpkts": int(flow.attrib["DstPkts"]) if "DstPkts" in flow.attrib else 0,
+            "stcpb": flow.attrib["SrcTCPBase"] if "SrcTCPBase" in flow.attrib else 0,
+            "dtcpb": flow.attrib["DstTCPBase"] if "DstTCPBase" in flow.attrib else 0,
+            "smean": flow.attrib["sMeanPktSz"] if "sMeanPktSz" in flow.attrib else 0,
+            "dmean": flow.attrib["dMeanPktSz"] if "dMeanPktSz" in flow.attrib else 0,
+            "trans_depth": int(flow.attrib["Trans"]) if "Trans" in flow.attrib else 0,
+            "sjit": float(flow.attrib["SrcJitter"])
+            if "SrcJitter" in flow.attrib
+            else 0,
+            "djit": float(flow.attrib["DstJitter"])
+            if "DstJitter" in flow.attrib
+            else 0,
+            "Stime": stime,
+            "Ltime": ltime,
+            "Sintpkt": flow.attrib["SIntPkt"] if "SIntPkt" in flow.attrib else 0,
+            "Dintpkt": flow.attrib["DIntPkt"] if "DIntPkt" in flow.attrib else 0,
+            "tcprtt": flow.attrib["TcpRtt"] if "TcpRtt" in flow.attrib else 0,
+            "synack": flow.attrib["SynAck"] if "SynAck" in flow.attrib else 0,
+            "ackdat": flow.attrib["AckDat"] if "AckDat" in flow.attrib else 0,
+            "ct_state_ttl": ct_state_ttl,
+            "ct_flw_http_mthd": 0,
+            "ct_srv_src": 0,
+            "ct_srv_dst": 0,
+            "ct_dst_ltm": 0,
+            "ct_src_ltm": 0,
         }
         return flow_dict
-    def process_output(self,line):
+
+    def process_output(self, line):
         try:
             # Parse the XML line
             root = ET.fromstring(line.strip())
-            if(root.tag == "ArgusFlowRecord"):
-                flow_dict = self.process_flow(root)    
+            if root.tag == "ArgusFlowRecord":
+                flow_dict = self.process_flow(root)
                 return flow_dict
         except ET.ParseError as e:
             print("Error parsing XML:", e)
-        
